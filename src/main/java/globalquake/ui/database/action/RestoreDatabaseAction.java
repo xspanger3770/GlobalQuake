5100b8de06f7342e59c00e5d89450e57ac42db8d
Rohan Udupa <86579136+lab596@users.noreply.github.com>
2023-08-17 09:18:35 -0700

    Adding Restore Icon

--- src/main/java/globalquake/ui/database/action/RestoreDatabaseAction.java
+++ src/main/java/globalquake/ui/database/action/RestoreDatabaseAction.java
@@ -17,6 +17,11 @@ public class RestoreDatabaseAction extends AbstractAction {
         this.parent = parent;
 
         putValue(SHORT_DESCRIPTION, "Restore everything to default");
+
+        ImageIcon restoreIcon = new ImageIcon(getClass().getResource("/image_icons/restore.png"));
+        Image image = restoreIcon.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(image);
+        putValue(Action.SMALL_ICON, scaledIcon);
     }
 
     @Override
