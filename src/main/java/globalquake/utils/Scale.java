de7eb913405be73ed761122292f66ed32f1cb835
Jakub Španger <jakubspanger1025@gmail.com>
2023-08-24 11:54:04 +0200

    intensity scale interpolation for smoother effect

--- src/main/java/globalquake/utils/Scale.java
+++ src/main/java/globalquake/utils/Scale.java
@@ -9,6 +9,7 @@ import java.util.Objects;
 public class Scale {
 
 	public static final double RATIO_TRESHOLD = 50_000.0;
+	public static final double EXPONENT = 0.266;
 
 	private static BufferedImage pgaScale;
 	public static void load() throws IOException, NullPointerException {
@@ -16,9 +17,45 @@ public class Scale {
 	}
 
 	public static Color getColorRatio(double ratio) {
-		double exp = 1 / 3.75;
-		double pct = Math.pow(ratio, exp) / Math.pow(50000, exp);
-		return new Color(pgaScale.getRGB(0, Math.max(0, Math.min(pgaScale.getHeight() - 1, (int)((pgaScale.getHeight() - 1) * pct)))));
+		double pct = Math.pow(ratio, EXPONENT) / Math.pow(RATIO_TRESHOLD, EXPONENT);
+
+		int i1 = (int)(pct * (pgaScale.getHeight() - 1));
+		int i2 = i1 + 1;
+
+		if(i1 < 0){
+			return new Color(pgaScale.getRGB(0, 0));
+		} else if (i1 >= pgaScale.getHeight() - 1){
+			return new Color(pgaScale.getRGB(0, pgaScale.getHeight() - 1));
+		}
+
+		double weight = (pct * (pgaScale.getHeight() - 1)) - i1;
+
+		Color color1 = new Color(pgaScale.getRGB(0, i1));
+		Color color2 = new Color(pgaScale.getRGB(0, i2));
+
+		return interpolateColors(color1, color2, weight);
+	}
+
+	public static Color interpolateColors(Color color1, Color color2, double weight) {
+		// Make sure weight is within the range [0, 1]
+		weight = Math.max(0, Math.min(1, weight));
+
+		// Extract RGB components of the two colors
+		int r1 = color1.getRed();
+		int g1 = color1.getGreen();
+		int b1 = color1.getBlue();
+
+		int r2 = color2.getRed();
+		int g2 = color2.getGreen();
+		int b2 = color2.getBlue();
+
+		// Interpolate RGB components
+		int rInterpolated = (int) (r1 * (1 - weight) + r2 * weight);
+		int gInterpolated = (int) (g1 * (1 - weight) + g2 * weight);
+		int bInterpolated = (int) (b1 * (1 - weight) + b2 * weight);
+
+		// Create the interpolated color
+		return new Color(rInterpolated, gInterpolated, bInterpolated);
 	}
 
 	public static Color getColorEasily(double ratio) {
