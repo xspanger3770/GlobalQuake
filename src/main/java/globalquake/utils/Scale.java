e1dee16d4f2cb50287240ce7399ba9fe5e1b57f6
Jakub Španger <jakubspanger1025@gmail.com>
2023-08-24 11:29:37 +0200

    update intensity scale to be more realistic and less exaggerated

--- src/main/java/globalquake/utils/Scale.java
+++ src/main/java/globalquake/utils/Scale.java
@@ -8,14 +8,17 @@ import java.util.Objects;
 
 public class Scale {
 
+	public static final double RATIO_TRESHOLD = 50_000.0;
+
 	private static BufferedImage pgaScale;
 	public static void load() throws IOException, NullPointerException {
 		pgaScale = ImageIO.read(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource("scales/pgaScale3.png")));
 	}
 
 	public static Color getColorRatio(double ratio) {
-		int i = (int) (Math.log10(ratio) * 20.0);
-		return new Color(pgaScale.getRGB(0, Math.max(0, Math.min(pgaScale.getHeight() - 1, i))));
+		double exp = 1 / 3.75;
+		double pct = Math.pow(ratio, exp) / Math.pow(50000, exp);
+		return new Color(pgaScale.getRGB(0, Math.max(0, Math.min(pgaScale.getHeight() - 1, (int)((pgaScale.getHeight() - 1) * pct)))));
 	}
 
 	public static Color getColorEasily(double ratio) {
