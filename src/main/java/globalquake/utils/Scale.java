9ba6627877608afdb2c45ce6bc8ad2075eb2ba68
Jakub Španger <jakubspanger1025@gmail.com>
2023-08-26 19:33:36 +0200

    switch back to new colors

--- src/main/java/globalquake/utils/Scale.java
+++ src/main/java/globalquake/utils/Scale.java
@@ -19,11 +19,6 @@ public class Scale {
 	}
 
 	public static Color getColorRatio(double ratio) {
-		int i = (int) (Math.log10(ratio) * 20.0);
-		return new Color(pgaScale.getRGB(0, Math.max(0, Math.min(pgaScale.getHeight() - 1, i))));
-	}
-
-	public static Color getColorRatioNew(double ratio) {
 		if(ratio < 1){
 			return new Color(pgaScale.getRGB(0, 0));
 		}
