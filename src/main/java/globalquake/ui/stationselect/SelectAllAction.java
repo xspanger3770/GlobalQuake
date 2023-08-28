11d6ce44db3a677802eb4669dd7ea4f554dd24aa
Rohan Udupa <86579136+lab596@users.noreply.github.com>
2023-08-28 12:22:27 -0700

    Updating button notification

--- src/main/java/globalquake/ui/stationselect/SelectAllAction.java
+++ src/main/java/globalquake/ui/stationselect/SelectAllAction.java
@@ -5,25 +5,43 @@ import globalquake.database.Station;
 import globalquake.database.StationDatabaseManager;
 
 import javax.swing.*;
+
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 
 public class SelectAllAction extends AbstractAction {
 
     private final StationDatabaseManager stationDatabaseManager;
+    private final Window parent;
 
-    public SelectAllAction(StationDatabaseManager stationDatabaseManager) {
+    public SelectAllAction(StationDatabaseManager stationDatabaseManager, Window parent) {
         super("Select All");
         this.stationDatabaseManager=stationDatabaseManager;
+        this.parent=parent;
 
         putValue(SHORT_DESCRIPTION, "Select All Available Stations");
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
+        boolean alreadySelected = true;
         stationDatabaseManager.getStationDatabase().getDatabaseWriteLock().lock();
         try{
             for(Network network : stationDatabaseManager.getStationDatabase().getNetworks()){
+                for(Station station : network.getStations()){
+                    if(station.getSelectedChannel() != null && !station.getSelectedChannel().isAvailable()){
+                        continue;
+                    }
+                    if(station.getSelectedChannel() == null){
+                        System.out.println("Selecting " + station.getStationCode());
+                        alreadySelected = false;
+                        break;
+                    }
+                }
                 network.getStations().forEach(Station::selectBestAvailableChannel);
+            }
+            if(alreadySelected){
+                JOptionPane.showMessageDialog(parent, "All Stations Already Selected");
             }
             stationDatabaseManager.fireUpdateEvent();
         }finally {
