ff89e26b28e1f186f8ca4256b65e3c2444f770a5
Rohan Udupa <86579136+lab596@users.noreply.github.com>
2023-08-30 09:30:28 -0700

    Adding Suggestions

--- src/main/java/globalquake/ui/stationselect/StationSelectFrame.java
+++ src/main/java/globalquake/ui/stationselect/StationSelectFrame.java
@@ -14,9 +14,8 @@ public class StationSelectFrame extends JFrame implements ActionListener {
     private final DatabaseMonitorFrame databaseMonitorFrame;
     private JToggleButton selectButton;
     private JToggleButton deselectButton;
-    private JButton selectAll;
-    private JButton deselectAll;
-    private boolean stationRepaint = false;
+    private final JButton selectAll;
+    private final JButton deselectAll;
     private DragMode dragMode = DragMode.NONE;
     private final JCheckBox chkBoxShowUnavailable;
 
@@ -24,40 +23,23 @@ public class StationSelectFrame extends JFrame implements ActionListener {
         setLayout(new BorderLayout());
         this.databaseMonitorFrame = databaseMonitorFrame;
         
-        JPanel togglePanel = new JPanel(new GridLayout(9,1));
+        JPanel togglePanel = new JPanel(new GridBagLayout());
         JButton toggleButton = new JButton("<");
         selectAll = new JButton(new SelectAllAction(databaseMonitorFrame.getManager()));
         deselectAll = new JButton(new DeselectAllAction(databaseMonitorFrame.getManager()));
         selectAll.addActionListener(this);
         deselectAll.addActionListener(this);
-        JPanel filler1 = new JPanel();
-        JPanel filler2 = new JPanel();
-        JPanel filler3 = new JPanel();
-        JPanel filler4 = new JPanel();
-        JPanel filler5 = new JPanel();
-        JPanel filler6 = new JPanel();
-        JPanel filler7 = new JPanel();
-        JPanel filler8 = new JPanel();
 
-        filler1.setOpaque(false);
-        filler2.setOpaque(false);
-        filler3.setOpaque(false);
-        filler4.setOpaque(false);
-        filler5.setOpaque(false);
-        filler6.setOpaque(false);
-        filler7.setOpaque(false);
-        filler8.setOpaque(false);
-
-        togglePanel.add(filler1);
-        togglePanel.add(filler2);
-        togglePanel.add(filler3);
-        togglePanel.add(filler4);
-        togglePanel.add(toggleButton);
-        togglePanel.add(filler5);
-        togglePanel.add(filler6);
-        togglePanel.add(filler7);
-        togglePanel.add(filler8);
         togglePanel.setOpaque(false);
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.ipady = 30;
+
+        togglePanel.add(toggleButton, gbc);
 
         toggleButton.setToolTipText("Toggle Toolbar");
         toggleButton.setBackground(Color.GRAY);
@@ -88,16 +70,16 @@ public class StationSelectFrame extends JFrame implements ActionListener {
                 }
             }
         });
-        JPanel CenterPanel = new JPanel(new GridBagLayout());
+        JPanel centerPanel = new JPanel(new GridBagLayout());
 
-        GridBagConstraints gbc = new GridBagConstraints();
+        gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.LINE_START;
         gbc.fill = GridBagConstraints.VERTICAL;
         gbc.weighty = 1.0;
 
-        CenterPanel.add(togglePanel, gbc);
+        centerPanel.add(togglePanel, gbc);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -106,9 +88,9 @@ public class StationSelectFrame extends JFrame implements ActionListener {
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
 
-        CenterPanel.add(stationSelectPanel, gbc);
+        centerPanel.add(stationSelectPanel, gbc);
 
-        add(CenterPanel, BorderLayout.CENTER);
+        add(centerPanel, BorderLayout.CENTER);
         add(toolBar, BorderLayout.WEST);
         add(new StationCountPanel(databaseMonitorFrame, new GridLayout(1,4)), BorderLayout.SOUTH);
 
@@ -118,12 +100,7 @@ public class StationSelectFrame extends JFrame implements ActionListener {
         setTitle("Select Stations");
 
         javax.swing.Timer timer = new javax.swing.Timer(16, actionEvent -> {
-            if(stationRepaint){
-                stationSelectPanel.repaint();
-            }
-            else{
-                toggleButton.repaint();
-            }
+            centerPanel.repaint();
         });
         timer.start();
     }
@@ -142,10 +119,8 @@ public class StationSelectFrame extends JFrame implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 if(selectButton.isSelected()){
-                    stationRepaint = true;
                     setDragMode(DragMode.SELECT);
                 } else {
-                    stationRepaint = false;
                     setDragMode(DragMode.NONE);
                 }
             }
@@ -155,10 +130,8 @@ public class StationSelectFrame extends JFrame implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 if(deselectButton.isSelected()){
-                    stationRepaint = true;
                     setDragMode(DragMode.DESELECT);
                 } else {
-                    stationRepaint = false;
                     setDragMode(DragMode.NONE);
                 }
             }
@@ -216,6 +189,5 @@ public class StationSelectFrame extends JFrame implements ActionListener {
             deselectAll.setEnabled(true);
             deselectButton.setEnabled(true);
         }
-        stationSelectPanel.repaint();
     }
 }
