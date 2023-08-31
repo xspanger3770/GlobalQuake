acf1aedb5ec4f54f5ab5c87cbb7dc65448500dfc
Jakub Španger <jakubspanger1025@gmail.com>
2023-08-31 12:43:26 +0200

    fix filter related issues in quake list, add button to scroll up

--- src/main/java/globalquake/intensity/MMIIntensityScale.java
+++ src/main/java/globalquake/intensity/MMIIntensityScale.java
@@ -35,7 +35,7 @@ public class MMIIntensityScale implements IntensityScale{
 		levels.add(I = new Level("I", 0.5, new Color(170,170,170)));
 		levels.add(II = new Level("II", 1.0, new Color(200, 190, 240)));
 		levels.add(III = new Level("III", 2.1, new Color(132, 162, 232)));
-		levels.add(IV = new Level("IV", 5.0, new Color(110, 214, 255)));
+		levels.add(IV = new Level("IV", 5.0, new Color(130, 214, 255)));
 		levels.add(V = new Level("V", 10.0, new Color(85, 242, 15)));
 		levels.add(VI = new Level("VI", 21.0, new Color(255, 255, 0)));
 		levels.add(VII = new Level("VII", 44.0, new Color(255, 200, 0)));
@@ -43,7 +43,7 @@ public class MMIIntensityScale implements IntensityScale{
 		levels.add(IX = new Level("IX", 202.0, new Color(255, 0, 0)));
 		levels.add(X = new Level("X", 432.0, new Color(190, 0, 0)));
 		levels.add(XI = new Level("XI", 923.0, new Color(130, 0, 0)));
-		levels.add(XII = new Level("XII", 1972.0, new Color(80, 0, 0)));
+		levels.add(XII = new Level("XII", 1972.0, new Color(65, 0, 0)));
 
 		levels.sort(Comparator.comparing(level -> -level.getPga()));
 	}
@@ -65,7 +65,7 @@ public class MMIIntensityScale implements IntensityScale{
 
 	@Override
 	public double getDarkeningFactor() {
-		return 0.70;
+		return 0.62;
 	}
 
 	@Override
--- src/main/java/globalquake/ui/globalquake/EarthquakeListPanel.java
+++ src/main/java/globalquake/ui/globalquake/EarthquakeListPanel.java
@@ -19,6 +19,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class EarthquakeListPanel extends JPanel {
     private double scroll = 0;
@@ -29,23 +30,49 @@ public class EarthquakeListPanel extends JPanel {
     public static final DecimalFormat f1d = new DecimalFormat("0.0", new DecimalFormatSymbols(Locale.ENGLISH));
     private static final int cell_height = 50;
 
+    private static Rectangle2D.Double goUpRectangle;
+
     private final List<ArchivedQuake> archivedQuakes;
+    private boolean isMouseInGoUpRect;
+
+    private List<ArchivedQuake> getFiltered(){
+        if(archivedQuakes == null){
+            return null;
+        }
+        return archivedQuakes.stream().filter( quake -> {
+            if (Settings.oldEventsMagnitudeFilterEnabled && quake.getMag() < Settings.oldEventsMagnitudeFilter) {
+                return false;
+            }
+
+            if (Settings.oldEventsTimeFilterEnabled && (System.currentTimeMillis() - quake.getOrigin()) > 1000 * 60 * 60L * Settings.oldEventsTimeFilter) {
+                return false;
+            }
+
+            return true;
+        }).collect(Collectors.toList());
+    }
 
     public EarthquakeListPanel(List<ArchivedQuake> archivedQuakes) {
         this.archivedQuakes = archivedQuakes;
         setBackground(Color.gray);
         setForeground(Color.gray);
 
+        goUpRectangle = new Rectangle2D.Double(getWidth() / 2.0 - 30, 0, 60, 26);
+
         addMouseWheelListener(e -> {
+            List<ArchivedQuake> filtered = getFiltered();
+            if(filtered == null){
+                return;
+            }
             boolean down = e.getWheelRotation() < 0;
+            scroll += e.getPreciseWheelRotation() * 30.0;
+
             if (!down) {
-                scroll += e.getPreciseWheelRotation() * 30.0;
-                int maxScroll = archivedQuakes.size() * cell_height
+                int maxScroll = filtered.size() * cell_height
                         - getHeight();
                 maxScroll = Math.max(0, maxScroll);
                 scroll = Math.min(scroll, maxScroll);
             } else {
-                scroll += e.getPreciseWheelRotation() * 30.0;
                 scroll = Math.max(0, scroll);
             }
         });
@@ -55,14 +82,19 @@ public class EarthquakeListPanel extends JPanel {
             public void mousePressed(MouseEvent e) {
                 int y = e.getY();
                 int i = (int) ((y + scroll) / cell_height);
-                if (archivedQuakes == null || i < 0 || i >= archivedQuakes.size()) {
+                List<ArchivedQuake> filtered = getFiltered();
+                if (filtered == null || i < 0 || i >=filtered.size()) {
                     return;
                 }
 
-                ArchivedQuake quake = archivedQuakes.get(i);
+                ArchivedQuake quake = filtered.get(i);
 
                 if (quake != null && e.getButton() == MouseEvent.BUTTON3) {
                     quake.setWrong(!quake.isWrong());
+                }
+
+                if(isMouseInGoUpRect && e.getButton() == MouseEvent.BUTTON1){
+                    scroll = 0;
                 }
             }
 
@@ -76,6 +108,7 @@ public class EarthquakeListPanel extends JPanel {
             @Override
             public void mouseMoved(MouseEvent e) {
                 mouseY = e.getY();
+                isMouseInGoUpRect = goUpRectangle.contains(e.getPoint());
             }
         });
     }
@@ -83,96 +116,93 @@ public class EarthquakeListPanel extends JPanel {
     @Override
     public void paint(Graphics gr) {
         super.paint(gr);
+        goUpRectangle = new Rectangle2D.Double(getWidth() / 2.0 - 30, 0, 60, 26);
         Graphics2D g = (Graphics2D) gr;
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         int i = 0;
-        for (ArchivedQuake quake : archivedQuakes) {
-            if(Settings.oldEventsMagnitudeFilterEnabled && quake.getMag() < Settings.oldEventsMagnitudeFilter){
-                continue;
-            }
-
-            if(Settings.oldEventsTimeFilterEnabled && (System.currentTimeMillis() - quake.getOrigin()) > 1000 * 60 * 60L *Settings.oldEventsTimeFilter){
-                continue;
-            }
-
-            int y = (int) (i * cell_height - scroll);
-            if (y > getHeight()) {
-                break;
-            }
-            Color col;
-            Level level = IntensityScales.getIntensityScale().getLevel(GeoUtils.pgaFunctionGen1(quake.getMag(), quake.getDepth()));
-            if (level != null) {
-                col = level.getColor();
+        if(getFiltered() != null) {
+            for (ArchivedQuake quake : getFiltered()) {
+                int y = (int) (i * cell_height - scroll);
+                if (y > getHeight()) {
+                    break;
+                }
+                Color col;
+                Level level = IntensityScales.getIntensityScale().getLevel(GeoUtils.pgaFunctionGen1(quake.getMag(), quake.getDepth()));
+                if (level != null) {
+                    col = level.getColor();
 
 
-                col = new Color(
-                        (int) (col.getRed() * IntensityScales.getIntensityScale().getDarkeningFactor()),
-                        (int) (col.getGreen() * IntensityScales.getIntensityScale().getDarkeningFactor()),
-                        (int) (col.getBlue() * IntensityScales.getIntensityScale().getDarkeningFactor()));
-            }else {
-                 col = new Color(150,150,150);
-            }
+                    col = new Color(
+                            (int) (col.getRed() * IntensityScales.getIntensityScale().getDarkeningFactor()),
+                            (int) (col.getGreen() * IntensityScales.getIntensityScale().getDarkeningFactor()),
+                            (int) (col.getBlue() * IntensityScales.getIntensityScale().getDarkeningFactor()));
+                } else {
+                    col = new Color(140, 140, 140);
+                }
 
-            Rectangle2D.Double rect = new Rectangle2D.Double(0, y, getWidth(), cell_height);
+                Rectangle2D.Double rect = new Rectangle2D.Double(0, y, getWidth(), cell_height);
 
-            g.setColor(col);
-            g.fill(rect);
-            g.setColor(Color.LIGHT_GRAY);
-            g.setStroke(new BasicStroke(0.5f));
-            g.draw(rect);
-
-            if (y / cell_height == mouseY / cell_height) {
-                g.setColor(new Color(0, 0, 0, 60));
+                g.setColor(col);
                 g.fill(rect);
-            }
+                g.setColor(Color.LIGHT_GRAY);
+                g.setStroke(new BasicStroke(0.5f));
+                g.draw(rect);
 
-            String str = "M" + f1d.format(quake.getMag());
-            g.setFont(new Font("Calibri", Font.BOLD, 20));
-            g.setColor(Color.WHITE);
-            g.drawString(str, getWidth() - g.getFontMetrics().stringWidth(str) - 3, y + 44);
+                if (!isMouseInGoUpRect && (int)((mouseY + scroll) / cell_height) == i) {
+                    g.setColor(new Color(0, 0, 0, 60));
+                    g.fill(rect);
+                }
 
-            if (level != null) {
-                str = level.getName();
-            } else {
-                str = "-";
-            }
+                String str = "M" + f1d.format(quake.getMag());
+                g.setFont(new Font("Calibri", Font.BOLD, 20));
+                g.setColor(Color.WHITE);
+                g.drawString(str, getWidth() - g.getFontMetrics().stringWidth(str) - 3, y + 44);
 
-            if (level != null) {
+                if (level != null) {
+                    str = level.getName();
+                } else {
+                    str = "-";
+                }
+
+                if (level != null) {
+                    g.setColor(Color.white);
+                    g.setFont(new Font("Arial", Font.PLAIN, 20));
+                    g.drawString(level.getSuffix(), 32, y + 21);
+
+                }
+
+                g.setFont(new Font("Calibri", Font.PLAIN, 26));
                 g.setColor(Color.white);
-                g.setFont(new Font("Arial", Font.PLAIN, 20));
-                g.drawString(level.getSuffix(), 32, y + 21);
+                g.drawString(str, 27 - g.getFontMetrics().stringWidth(str) / 2, y + 30);
 
+                str = ((int) quake.getDepth()) + "km";
+                g.setFont(new Font("Calibri", Font.BOLD, 12));
+                g.setColor(Color.white);
+                g.drawString(str, (int) (25 - g.getFontMetrics().stringWidth(str) * 0.5), y + 46);
+
+                str = quake.getRegion();
+                g.setFont(new Font("Calibri", Font.BOLD, 12));
+                g.setColor(Color.white);
+                g.drawString(str, 52, y + 18);
+
+                str = formatNice.format(Instant.ofEpochMilli(quake.getOrigin()));
+                g.setFont(new Font("Calibri", Font.PLAIN, 16));
+                g.setColor(Color.white);
+                g.drawString(str, 52, y + 42);
+
+                if (quake.isWrong()) {
+                    g.setColor(new Color(200, 0, 0));
+                    g.setStroke(new BasicStroke(2f));
+                    int r = 5;
+                    g.drawLine(r, y + r, getWidth() - r, y + cell_height - r);
+                    g.drawLine(r, y + cell_height - r, getWidth() - r, y + r);
+                }
+
+                i++;
             }
-
-            g.setFont(new Font("Calibri", Font.PLAIN, 26));
-            g.setColor(Color.white);
-            g.drawString(str, 27-g.getFontMetrics().stringWidth(str) / 2, y + 30);
-
-            str = ((int) quake.getDepth()) + "km";
-            g.setFont(new Font("Calibri", Font.BOLD, 12));
-            g.setColor(Color.white);
-            g.drawString(str, (int) (25 - g.getFontMetrics().stringWidth(str) * 0.5), y + 46);
-
-            str = quake.getRegion();
-            g.setFont(new Font("Calibri", Font.BOLD, 12));
-            g.setColor(Color.white);
-            g.drawString(str, 52, y + 18);
-
-            str = formatNice.format(Instant.ofEpochMilli(quake.getOrigin()));
-            g.setFont(new Font("Calibri", Font.PLAIN, 16));
-            g.setColor(Color.white);
-            g.drawString(str, 52, y + 42);
-
-            if (quake.isWrong()) {
-                g.setColor(new Color(200, 0, 0));
-                g.setStroke(new BasicStroke(2f));
-                int r = 5;
-                g.drawLine(r, y + r, getWidth() - r, y + cell_height - r);
-                g.drawLine(r, y + cell_height - r, getWidth() - r, y + r);
-            }
-
-            i++;
         }
+
+        g.setStroke(new BasicStroke(1f));
 
         if(i == 0){
             g.setFont(new Font("Calibri", Font.BOLD, 16));
@@ -181,11 +211,20 @@ public class EarthquakeListPanel extends JPanel {
             g.drawString(str, getWidth() / 2 - g.getFontMetrics().stringWidth(str) / 2, 22);
         }
 
-        // workaround for max scroll
-        int maxScroll = i * cell_height
-                - getHeight();
-        maxScroll = Math.max(0, maxScroll);
-        scroll = Math.min(scroll, maxScroll);
+        if(scroll > 0){
+            g.setColor(Color.gray);
+            g.fill(goUpRectangle);
+
+            if(isMouseInGoUpRect){
+                g.setStroke(new BasicStroke(2f));
+            }
+
+            g.setColor(Color.white);
+            g.draw(goUpRectangle);
+            g.setFont(new Font("Calibri", !isMouseInGoUpRect ? Font.PLAIN : Font.BOLD, 32));
+            String str = "^";
+            g.drawString(str, getWidth() / 2 - g.getFontMetrics().stringWidth(str) / 2, 30);
+        }
     }
 
 }
