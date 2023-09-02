2b7047526e7d408b406756dba2c6e8ba7ae53740
Rohan Udupa <86579136+lab596@users.noreply.github.com>
2023-09-02 15:56:13 -0700

    Update SearchBar.java

--- src/main/java/globalquake/ui/stationselect/SearchBar.java
+++ src/main/java/globalquake/ui/stationselect/SearchBar.java
@@ -1,5 +1,6 @@
 package globalquake.ui.stationselect;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
@@ -11,16 +12,25 @@ import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Point;
 import java.awt.RenderingHints;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.geom.Ellipse2D;
+
+import javax.swing.Timer;
 
 import javax.swing.Icon;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 public class SearchBar extends JTextField{
     
@@ -30,6 +40,11 @@ public class SearchBar extends JTextField{
     private final Icon iconClose;
     private final Icon iconLoading;
     private final String hintText = "Searching ... ";
+    private String search = "";
+    private Timer timer;
+    private boolean show;
+    private float speed = 8f;
+    private float location = -1;
 
 
     public SearchBar(){
@@ -58,7 +73,46 @@ public class SearchBar extends JTextField{
             public void mousePressed(MouseEvent me){
                 if(SwingUtilities.isLeftMouseButton(me)){
                     if(checkMouseOver(me.getPoint())){
-                        System.out.print("Mouse Pressed");
+                        //Mouse Pressed over the button
+                        search = getText();
+                        if(!timer.isRunning()){
+                            if(show){
+                                setEditable(true);
+                                show = false;
+                                location = -1;
+                                timer.start();
+                            }
+                            else{
+                                setEditable(false);
+                                show = true;
+                                location = getWidth();
+                                timer.start();
+                            }
+                        }
+                        parseSearch(search);
+                    }
+                }
+            }
+        });
+        timer=new Timer(0, new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent ae) {
+                if(show){
+                    if(location>0){
+                        location-=speed;
+                        repaint();
+                    }
+                    else{
+                        timer.stop();
+                    }
+                }
+                else{
+                    if(location<getWidth()){
+                        location+=speed;
+                        repaint();
+                    }
+                    else{
+                        timer.stop();
                     }
                 }
             }
@@ -78,16 +132,36 @@ public class SearchBar extends JTextField{
         setPreferredSize(new Dimension(width, height));
         g2.fillRoundRect(0, 0, width, height, height, height);
         super.paintComponent(g);
-
+        //Create Button
         int marginButton = 5;
         int buttonSize = height - marginButton * 2;
         GradientPaint gp = new GradientPaint(0, 0, new Color(255,255,255,0), width, 0, animationColor);
         g2.setPaint(gp);
         g2.fillOval((width - height)+3, marginButton, buttonSize, buttonSize);
+
+        //Create Animation
+        if(location != -1){
+            g2.fillRoundRect((int)location, 0, (int)(width-location), height, height, height);
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, getAlpha()));
+            //Create Loading Icon
+            int iconWidth = 30;
+            int iconHeight = 30;
+            g2.drawImage(((javax.swing.ImageIcon) iconLoading).getImage(), (int)(location-5), (height-iconHeight)/2, iconWidth, iconHeight, this);
+        }
+        
+        //Create Button Icon
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
         int marginImage=5;
         int imageSize = buttonSize - marginImage * 2;
-
-        Image image = ((javax.swing.ImageIcon) iconSearch).getImage();
+        Image image;
+        if(show){
+            image = ((javax.swing.ImageIcon) iconClose).getImage();
+            search = "";
+            setText("");
+        }
+        else{
+            image = ((javax.swing.ImageIcon) iconSearch).getImage();
+        }
         g2.drawImage(image, width-height+marginImage + 3, marginButton+marginImage, imageSize, imageSize, null);
         g2.dispose();
     }
@@ -108,7 +182,18 @@ public class SearchBar extends JTextField{
             g.drawString(hintText, ins.left, h / 2 + fm.getAscent() / 2 - 2);
         }
     }
-
+    private float getAlpha(){
+        float width = getWidth()/2;
+        float alpha = (location)/(-width);
+        alpha+=1;
+        if(alpha<0){
+            alpha = 0f;
+        }
+        if(alpha>1){
+            alpha = 1f;
+        }
+        return alpha;
+    }
     private boolean checkMouseOver(Point mouse){
         int width = getWidth();
         int height = getHeight();
@@ -116,6 +201,32 @@ public class SearchBar extends JTextField{
         Point point = new Point(width-height+3, 5);
         Ellipse2D.Double circle = new Ellipse2D.Double(point.x, point.y, buttonSize, buttonSize);
         return circle.contains(mouse);
+    }
+
+    private void parseSearch(String search){
+        try{
+            JSONParser parser = new JSONParser();
+            JSONObject rootObject = (JSONObject) parser.parse(new FileReader("src/main/resources/polygons/countriesHD.json"));
+
+            JSONArray countryData = (JSONArray) rootObject.get("features");
+           
+            String userInput = search.toLowerCase();
+            userInput = userInput.substring(0, 1).toUpperCase() + userInput.substring(1);
+
+            for(Object obj: countryData){
+                JSONObject countryObject = (JSONObject) obj;
+                JSONObject properties = (JSONObject) countryObject.get("properties");
+                String countryName = (String) properties.get("name");
+
+                if(countryName.equals(userInput)){
+                    System.out.println("Found country: " + countryName);
+                }
+
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error: " + e);
+        }
     }
     
 }
