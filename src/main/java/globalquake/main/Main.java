07b0bf0d48f29eba91d45310cbe2edec5550f5fc
Jakub Španger <jakubspanger1025@gmail.com>
2023-09-03 17:09:42 +0200

    set version to 0.9.6_pre2

--- src/main/java/globalquake/main/Main.java
+++ src/main/java/globalquake/main/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
     private static ApplicationErrorHandler errorHandler;
 
-    public static final String version = "0.9.6_pre1";
+    public static final String version = "0.9.6_pre2";
     public static final String fullName = "GlobalQuake " + version;
 
     public static final File MAIN_FOLDER = new File("./GlobalQuake/");
