63b06c33e559a9ea7158f6b4935fddef046d5a22
Jakub Španger <jakubspanger1025@gmail.com>
2023-09-03 11:55:49 +0200

    improve Settings to be easily expandable

--- src/main/java/globalquake/ui/settings/Settings.java
+++ src/main/java/globalquake/ui/settings/Settings.java
@@ -1,12 +1,16 @@
 package globalquake.ui.settings;
 
+import globalquake.core.analysis.Log;
 import globalquake.main.Main;
+import org.tinylog.Logger;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
+
+import java.lang.reflect.Field;
 
 public final class Settings {
 
@@ -63,72 +67,101 @@ public final class Settings {
 		} catch (IOException e) {
 			System.out.println("Created GlobalQuake properties file at "+optionsFile.getAbsolutePath());
 		}
-		
-		enableAlarmDialogs = Boolean.valueOf((String) properties.getOrDefault("enableAlarmDialogs", "false"));
-		
-		homeLat = Double.valueOf((String) properties.getOrDefault("homeLat", "0.0"));
-		homeLon = Double.valueOf((String) properties.getOrDefault("homeLon", "0.0"));
-		displayArchivedQuakes = Boolean.valueOf((String) properties.getOrDefault("displayArchivedQuakes", "true"));
-		enableSound = Boolean.valueOf((String) properties.getOrDefault("enableSound", "true"));
 
-		pWaveInaccuracyThreshold = Double.valueOf((String) properties.getOrDefault("pWaveInaccuracyThreshold", String.valueOf(pWaveInaccuracyThresholdDefault)));
-		hypocenterCorrectThreshold = Double.valueOf((String) properties.getOrDefault("hypocenterCorrectThreshold", String.valueOf(hypocenterCorrectThresholdDefault)));
-		hypocenterDetectionResolution = Double.valueOf((String) properties.getOrDefault("hypocenterDetectionResolution", String.valueOf(hypocenterDetectionResolutionDefault)));
-		minimumStationsForEEW = Integer.valueOf((String) properties.getOrDefault("minimumStationsForEEW", String.valueOf(minimumStationsForEEWDefault)));
-
-		useOldColorScheme = Boolean.valueOf((String) properties.getOrDefault("useOldColorScheme", "false"));
-		parallelHypocenterLocations = Boolean.valueOf((String) properties.getOrDefault("parallelHypocenterLocations", "false"));
-		displayHomeLocation = Boolean.valueOf((String) properties.getOrDefault("displayHomeLocation", "true"));
-		antialiasing = Boolean.valueOf((String) properties.getOrDefault("antialiasing", "false"));
-		fpsIdle = Integer.valueOf((String) properties.getOrDefault("fpsIdle", "30"));
-
-		intensityScaleIndex = Integer.valueOf((String) properties.getOrDefault("intensityScaleIndex", "0"));
-
-		oldEventsTimeFilterEnabled = Boolean.valueOf((String) properties.getOrDefault("oldEventsTimeFilterEnabled", "false"));
-		oldEventsTimeFilter = Double.valueOf((String) properties.getOrDefault("oldEventsTimeFilter", "24.0"));
-
-		oldEventsMagnitudeFilterEnabled = Boolean.valueOf((String) properties.getOrDefault("oldEventsMagnitudeFilterEnabled", "false"));
-		oldEventsMagnitudeFilter = Double.valueOf((String) properties.getOrDefault("oldEventsMagnitudeFilter", "4.0"));
-
-		oldEventsOpacity = Double.valueOf((String) properties.getOrDefault("oldEventsOpacity", "100.0"));
+		loadProperty("enableAlarmDialogs", "false");
+		loadProperty("homeLat", "0.0");
+		loadProperty("homeLon", "0.0");
+		loadProperty("displayArchivedQuakes", "true");
+		loadProperty("enableSound", "true");
+		loadProperty("pWaveInaccuracyThreshold", String.valueOf(pWaveInaccuracyThresholdDefault));
+		loadProperty("hypocenterCorrectThreshold", String.valueOf(hypocenterCorrectThresholdDefault));
+		loadProperty("hypocenterDetectionResolution", String.valueOf(hypocenterDetectionResolutionDefault));
+		loadProperty("minimumStationsForEEW", String.valueOf(minimumStationsForEEWDefault));
+		loadProperty("useOldColorScheme", "false");
+		loadProperty("parallelHypocenterLocations", "false");
+		loadProperty("displayHomeLocation", "true");
+		loadProperty("antialiasing", "false");
+		loadProperty("fpsIdle", "30");
+		loadProperty("intensityScaleIndex", "0");
+		loadProperty("oldEventsTimeFilterEnabled", "false");
+		loadProperty("oldEventsTimeFilter", "24.0");
+		loadProperty("oldEventsMagnitudeFilterEnabled", "false");
+		loadProperty("oldEventsMagnitudeFilter", "4.0");
+		loadProperty("oldEventsOpacity", "100.0");
 
 		save();
 	}
-	
+
+	private static void setProperty(Field field, Object value) {
+		try {
+			field.set(null, value);
+		} catch (Exception e) {
+			Logger.error(e);
+		}
+	}
+
+	private static void loadProperty(String name, String defaultVal){
+		try {
+			Field field = Settings.class.getDeclaredField(name);
+			field.setAccessible(true);
+			if (field.getType() == Boolean.class) {
+				boolean val;
+				try{
+					val = Boolean.parseBoolean((String) properties.getOrDefault(field.getName(), defaultVal));
+				}catch(Exception e){
+					Logger.error(e);
+					val = Boolean.parseBoolean(defaultVal);
+				}
+				setProperty(field, val);
+			} else if (field.getType() == Double.class) {
+				double val;
+				try{
+					val = Double.parseDouble((String) properties.getOrDefault(field.getName(), defaultVal));
+				}catch(Exception e){
+					Logger.error(e);
+					val = Double.parseDouble(defaultVal);
+				}
+				setProperty(field, val);
+			} else if (field.getType() == Integer.class) {
+				int val;
+				try{
+					val = Integer.parseInt((String) properties.getOrDefault(field.getName(), defaultVal));
+				}catch(Exception e){
+					Logger.error(e);
+					val = Integer.parseInt(defaultVal);
+				}
+				setProperty(field, val);
+			}
+		} catch (Exception e) {
+			Logger.error(e);
+		}
+	}
+
+	private static Object getPropertyValue(Field field){
+		try {
+			field.setAccessible(true);
+			return field.get(null);
+		} catch (Exception e) {
+			Logger.error(e);
+			return null;
+		}
+	}
 	
 	public static void save() {
 		changes++;
-		properties.setProperty("enableAlarmDialogs", String.valueOf(enableAlarmDialogs));
-		
-		properties.setProperty("homeLat", String.valueOf(homeLat));
-		properties.setProperty("homeLon", String.valueOf(homeLon));
-		properties.setProperty("displayArchivedQuakes", String.valueOf(displayArchivedQuakes));
-		properties.setProperty("enableSound", String.valueOf(enableSound));
-
-		properties.setProperty("pWaveInaccuracyThreshold", String.valueOf(pWaveInaccuracyThreshold));
-		properties.setProperty("hypocenterCorrectThreshold", String.valueOf(hypocenterCorrectThreshold));
-		properties.setProperty("hypocenterDetectionResolution", String.valueOf(hypocenterDetectionResolution));
-		properties.setProperty("minimumStationsForEEW", String.valueOf(minimumStationsForEEW));
-
-		properties.setProperty("useOldColorScheme", String.valueOf(useOldColorScheme));
-		properties.setProperty("parallelHypocenterLocations", String.valueOf(parallelHypocenterLocations));
-		properties.setProperty("displayHomeLocation", String.valueOf(displayHomeLocation));
-		properties.setProperty("antialiasing", String.valueOf(antialiasing));
-		properties.setProperty("fpsIdle", String.valueOf(fpsIdle));
-
-		properties.setProperty("intensityScaleIndex", String.valueOf(intensityScaleIndex));
-
-		properties.setProperty("oldEventsTimeFilterEnabled", String.valueOf(oldEventsTimeFilterEnabled));
-		properties.setProperty("oldEventsTimeFilter", String.valueOf(oldEventsTimeFilter));
-		properties.setProperty("oldEventsMagnitudeFilterEnabled", String.valueOf(oldEventsMagnitudeFilterEnabled));
-		properties.setProperty("oldEventsMagnitudeFilter", String.valueOf(oldEventsMagnitudeFilter));
-
-		properties.setProperty("oldEventsOpacity", String.valueOf(oldEventsOpacity));
 
 		try {
+			Field[] fields = Settings.class.getDeclaredFields();
+			for (Field field : fields) {
+				if (field.getType() == Boolean.class || field.getType() == Double.class || field.getType() == Integer.class) {
+					String value = String.valueOf(getPropertyValue(field));
+					properties.setProperty(field.getName(), value);
+				}
+			}
 			properties.store(new FileOutputStream(optionsFile), "Fun fact: I've never felt an earthquake in my life");
 		} catch (IOException e) {
 			Main.getErrorHandler().handleException(e);
 		}
+
 	}
 }
