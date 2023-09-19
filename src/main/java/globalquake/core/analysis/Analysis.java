2e9cb32a37995a030ae9113c777e8d3d36248547
Jakub Španger <jakubspanger1025@gmail.com>
2023-09-19 11:59:23 +0200

    fix ArrayIndexOutOfBoundsException in Analysis.java

--- src/main/java/globalquake/core/analysis/Analysis.java
+++ src/main/java/globalquake/core/analysis/Analysis.java
@@ -10,122 +10,119 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public abstract class Analysis {
-	private long lastRecord;
-	private final AbstractStation station;
-	private double sampleRate;
-	private final List<Event> detectedEvents;
-	public long numRecords;
-	public long latestLogTime;
-	public double _maxRatio;
-	public boolean _maxRatioReset;
-	public final Object previousLogsLock;
-	private final ArrayList<Log> previousLogs;
-	private AnalysisStatus status;
-	
-	public Analysis(AbstractStation station) {
-		this.station = station;
-		this.sampleRate = -1;
-		detectedEvents = new CopyOnWriteArrayList<>();
-		previousLogsLock = new Object();
-		previousLogs = new ArrayList<>();
-		status = AnalysisStatus.IDLE;
-	}
+    private long lastRecord;
+    private final AbstractStation station;
+    private double sampleRate;
+    private final List<Event> detectedEvents;
+    public long numRecords;
+    public long latestLogTime;
+    public double _maxRatio;
+    public boolean _maxRatioReset;
+    public final Object previousLogsLock;
+    private final ArrayList<Log> previousLogs;
+    private AnalysisStatus status;
 
-	public long getLastRecord() {
-		return lastRecord;
-	}
+    public Analysis(AbstractStation station) {
+        this.station = station;
+        this.sampleRate = -1;
+        detectedEvents = new CopyOnWriteArrayList<>();
+        previousLogsLock = new Object();
+        previousLogs = new ArrayList<>();
+        status = AnalysisStatus.IDLE;
+    }
 
-	public AbstractStation getStation() {
-		return station;
-	}
+    public long getLastRecord() {
+        return lastRecord;
+    }
 
-	public void analyse(DataRecord dr) {
-		if (sampleRate <= 0) {
-			sampleRate = dr.getSampleRate();
-			reset();
-		}
+    public AbstractStation getStation() {
+        return station;
+    }
+
+    public void analyse(DataRecord dr) {
+        if (sampleRate <= 0) {
+            sampleRate = dr.getSampleRate();
+            reset();
+        }
 
 
-		long time = dr.getLastSampleBtime().toInstant().toEpochMilli();
+        long time = dr.getLastSampleBtime().toInstant().toEpochMilli();
         if (time >= lastRecord && time <= System.currentTimeMillis() + 60 * 1000) {
             decode(dr);
             lastRecord = time;
         }
     }
 
-	private void decode(DataRecord dataRecord) {
-		long time = dataRecord.getStartBtime().toInstant().toEpochMilli();
-		long gap = lastRecord != 0 ? (time - lastRecord) : -1;
-		if (gap > getGapThreshold()) {
-			reset();
-		}
-		int[] data;
-		try {
-			if(!dataRecord.isDecompressable()){
-				Logger.warn("Not Decompressable!");
-				return;
-			}
-			data = dataRecord.decompress().getAsInt();
-			if(data == null){
-				Logger.warn("Decompressed array is null!");
-				return;
-			}
-
-			for (int v : data) {
-				nextSample(v, time, System.currentTimeMillis());
-				time += (long) (1000 / getSampleRate());
-			}
-		} catch (Exception e) {
-			Logger.warn("Crash occurred at station " + getStation().getStationCode() + ", thread continues.");
-			Logger.warn(e);
+    private void decode(DataRecord dataRecord) {
+        long time = dataRecord.getStartBtime().toInstant().toEpochMilli();
+        long gap = lastRecord != 0 ? (time - lastRecord) : -1;
+        if (gap > getGapThreshold()) {
+            reset();
         }
-	}
+        int[] data;
+        try {
+            if (!dataRecord.isDecompressable()) {
+                Logger.warn("Not Decompressable!");
+                return;
+            }
+            data = dataRecord.decompress().getAsInt();
+            if (data == null) {
+                Logger.warn("Decompressed array is null!");
+                return;
+            }
 
-	public abstract void nextSample(int v, long time, long currentTime);
+            for (int v : data) {
+                nextSample(v, time, System.currentTimeMillis());
+                time += (long) (1000 / getSampleRate());
+            }
+        } catch (Exception e) {
+            Logger.warn("Crash occurred at station " + getStation().getStationCode() + ", thread continues.");
+            Logger.warn(e);
+        }
+    }
 
-	@SuppressWarnings("SameReturnValue")
-	public abstract long getGapThreshold();
+    public abstract void nextSample(int v, long time, long currentTime);
 
-	public void reset() {
-		station.reset();
-	}
+    @SuppressWarnings("SameReturnValue")
+    public abstract long getGapThreshold();
 
-	public double getSampleRate() {
-		return sampleRate;
-	}
+    public void reset() {
+        station.reset();
+    }
 
-	public void setSampleRate(double sampleRate) {
-		this.sampleRate = sampleRate;
-	}
+    public double getSampleRate() {
+        return sampleRate;
+    }
 
-	public abstract void second(long time);
+    public void setSampleRate(double sampleRate) {
+        this.sampleRate = sampleRate;
+    }
 
-	public List<Event> getDetectedEvents() {
-		return detectedEvents;
-	}
+    public abstract void second(long time);
 
-	public Event getLatestEvent() {
-		if (detectedEvents.isEmpty()) {
-			return null;
-		} else {
-			return detectedEvents.get(0);
-		}
-	}
+    public List<Event> getDetectedEvents() {
+        return detectedEvents;
+    }
 
-	public long getNumRecords() {
-		return numRecords;
-	}
-	
-	public ArrayList<Log> getPreviousLogs() {
-		return previousLogs;
-	}
-	
-	public AnalysisStatus getStatus() {
-		return status;
-	}
-	
-	public void setStatus(AnalysisStatus status) {
-		this.status = status;
-	}
+    public Event getLatestEvent() {
+        var maybeEvent = detectedEvents.stream().findFirst();
+        return maybeEvent.orElse(null);
+    }
+
+    public long getNumRecords() {
+        return numRecords;
+    }
+
+    public ArrayList<Log> getPreviousLogs() {
+        return previousLogs;
+    }
+
+    public AnalysisStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AnalysisStatus status) {
+        this.status = status;
+    }
 
 }
