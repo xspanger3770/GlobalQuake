cc8b9e14f94df28d4e4f237a8b0df0118bdf0d97
Jakub Španger <jakubspanger1025@gmail.com>
2023-10-02 22:11:51 +0200

    version pre11

--- src/main/java/globalquake/main/Main.java
+++ src/main/java/globalquake/main/Main.java
@@ -25,7 +25,7 @@ public class Main {
 
     private static ApplicationErrorHandler errorHandler;
 
-    public static final String version = "0.9.8_pre10";
+    public static final String version = "0.9.8_pre11";
     public static final String fullName = "GlobalQuake " + version;
 
     public static final File MAIN_FOLDER = new File("./GlobalQuake/");
