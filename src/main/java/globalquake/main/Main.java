64e8f2d0d0a23271abf121389283a6b3f704c3c5
Jakub Španger <jakubspanger1025@gmail.com>
2023-10-10 09:08:32 +0000

    version 0.9.8_rc2

--- src/main/java/globalquake/main/Main.java
+++ src/main/java/globalquake/main/Main.java
@@ -24,7 +24,7 @@ import java.util.stream.Collectors;
 public class Main {
 
     private static ApplicationErrorHandler errorHandler;
-    public static final String version = "0.9.8_rc1";
+    public static final String version = "0.9.8_rc2";
     public static final String fullName = "GlobalQuake " + version;
     public static final File MAIN_FOLDER = new File("./GlobalQuake/");
     private static DatabaseMonitorFrame databaseMonitorFrame;
