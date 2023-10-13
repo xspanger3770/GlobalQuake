9d9bf4694e69fd76f63342798899444908c262c5
Jakub Španger <jakubspanger1025@gmail.com>
2023-10-13 22:00:40 +0000

    playing with h3 library

---
+++ src/test/java/globalquake/geo/taup/HexTest.java
@@ -0,0 +1,22 @@
+package globalquake.geo.taup;
+
+import com.uber.h3core.H3Core;
+import com.uber.h3core.util.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+
+public class HexTest {
+
+    public static void main(String[] args) throws IOException {
+        H3Core h3 = H3Core.newInstance();
+
+        double lat = 50.262;
+        double lon = 17.262;
+        int res = 4;
+
+        System.out.println(h3.latLngToCellAddress(lat, lon, res));
+        System.out.println(h3.gridDisk(h3.latLngToCellAddress(lat, lon, res), 2).size());
+    }
+
+}
