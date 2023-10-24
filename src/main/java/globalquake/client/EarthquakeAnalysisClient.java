5694669ecc6a5ca22026f9fbea48ef1e6c5aa8c8
Jakub Španger <jakubspanger1025@gmail.com>
2023-10-24 18:23:58 +0000

    accept advanced earthquake data

--- src/main/java/globalquake/client/EarthquakeAnalysisClient.java
+++ src/main/java/globalquake/client/EarthquakeAnalysisClient.java
@@ -6,22 +6,32 @@ import globalquake.core.earthquake.EarthquakeAnalysis;
 import globalquake.core.earthquake.data.Cluster;
 import globalquake.core.earthquake.data.Earthquake;
 import globalquake.core.earthquake.data.Hypocenter;
+import globalquake.core.earthquake.interval.DepthConfidenceInterval;
+import globalquake.core.earthquake.interval.PolygonConfidenceInterval;
+import globalquake.core.earthquake.quality.Quality;
 import globalquake.core.events.specific.QuakeCreateEvent;
 import globalquake.core.events.specific.QuakeRemoveEvent;
 import globalquake.core.events.specific.QuakeUpdateEvent;
+import globalquake.local.GlobalQuakeLocal;
 import gqserver.api.Packet;
 import gqserver.api.data.earthquake.EarthquakeInfo;
 import gqserver.api.data.earthquake.HypocenterData;
+import gqserver.api.data.earthquake.advanced.AdvancedHypocenterData;
+import gqserver.api.data.earthquake.advanced.DepthConfidenceIntervalData;
+import gqserver.api.data.earthquake.advanced.HypocenterQualityData;
+import gqserver.api.data.earthquake.advanced.LocationConfidenceIntervalData;
 import gqserver.api.packets.earthquake.EarthquakeCheckPacket;
 import gqserver.api.packets.earthquake.EarthquakeRequestPacket;
 import gqserver.api.packets.earthquake.HypocenterDataPacket;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 
 public class EarthquakeAnalysisClient extends EarthquakeAnalysis {
 
@@ -64,28 +74,60 @@ public class EarthquakeAnalysisClient extends EarthquakeAnalysis {
         ClientEarthquake existingQuake = clientEarthquakeMap.get(uuid);
         HypocenterData data = hypocenterData.data();
 
-        ClientEarthquake newQuake = createEarthquake(data);
+        ClientEarthquake newQuake = createEarthquake(data, hypocenterData.advancedHypocenterData());
 
         if(existingQuake == null) {
             clientEarthquakeMap.put(uuid, newQuake);
             earthquakes.add(newQuake);
-            GlobalQuake.instance.getEventHandler().fireEvent(new QuakeCreateEvent(newQuake));
+            GlobalQuakeLocal.instance.getEventHandler().fireEvent(new QuakeCreateEvent(newQuake));
         } else if(existingQuake.getRevisionID() < data.revisionID()) {
             existingQuake.update(newQuake);
             GlobalQuake.instance.getEventHandler().fireEvent(new QuakeUpdateEvent(existingQuake, null));
         }
     }
 
-    private ClientEarthquake createEarthquake(HypocenterData hypocenterData) {
+    private ClientEarthquake createEarthquake(HypocenterData hypocenterData, AdvancedHypocenterData advancedHypocenterData) {
         Hypocenter hypocenter = new Hypocenter(hypocenterData.lat(), hypocenterData.lon(), hypocenterData.depth(), hypocenterData.origin(),
-            0,0,null,null);
+            0,0,createDepthConfidenceInterval(advancedHypocenterData.depthIntervalData()),
+                createPolygonConfidenceIntervals(advancedHypocenterData.locationConfidenceIntervalData()));
 
         hypocenter.magnitude = hypocenterData.magnitude();
 
         Cluster cluster = new Cluster(0);
         cluster.revisionID = hypocenterData.revisionID();
 
+        if(advancedHypocenterData != null){
+            hypocenter.quality = createQuality(advancedHypocenterData.qualityData());
+        }
+
         cluster.setPreviousHypocenter(hypocenter);
         return new ClientEarthquake(cluster, hypocenterData.uuid());
+    }
+
+    private List<PolygonConfidenceInterval> createPolygonConfidenceIntervals(LocationConfidenceIntervalData locationConfidenceIntervalData) {
+        List<PolygonConfidenceInterval> result = new ArrayList<>();
+
+        for(var interval : locationConfidenceIntervalData.polygonConfidenceIntervalDataList()){
+            result.add(new PolygonConfidenceInterval(interval.n(), interval.offset(), interval.lengths().stream().map(Float::doubleValue).collect(Collectors.toList()),
+                    0, 0));
+        }
+
+        return result;
+    }
+
+    private DepthConfidenceInterval createDepthConfidenceInterval(DepthConfidenceIntervalData depthConfidenceIntervalData) {
+        return new DepthConfidenceInterval(
+                depthConfidenceIntervalData.minDepth(),
+                depthConfidenceIntervalData.maxDepth());
+    }
+
+    private Quality createQuality(HypocenterQualityData hypocenterQualityData) {
+        return new Quality(
+                hypocenterQualityData.errOrigin(),
+                hypocenterQualityData.errDepth(),
+                hypocenterQualityData.errNS(),
+                hypocenterQualityData.errEW(),
+                hypocenterQualityData.stations(),
+                hypocenterQualityData.pct());
     }
 }
