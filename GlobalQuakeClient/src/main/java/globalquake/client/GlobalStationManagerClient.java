65a9ae1e18d22834ccbada14c92c8f2ca266b08b
Jakub Španger <jakubspanger1025@gmail.com>
2023-12-03 20:19:54 +0100

    client: remove old waveforms

--- GlobalQuakeClient/src/main/java/globalquake/client/GlobalStationManagerClient.java
+++ GlobalQuakeClient/src/main/java/globalquake/client/GlobalStationManagerClient.java
@@ -64,6 +64,7 @@ public class GlobalStationManagerClient extends GlobalStationManager {
         try {
             DataRecord dataRecord = (DataRecord) DataRecord.read(dataRecordPacket.data());
             station.getAnalysis().analyse(dataRecord);
+            station.getAnalysis().second(System.currentTimeMillis());
         } catch (IOException | SeedFormatException e) {
             Logger.error(e);
         }
