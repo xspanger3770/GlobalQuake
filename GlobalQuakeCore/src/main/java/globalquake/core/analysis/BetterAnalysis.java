1b10e5683141397abdfa3f768b9d9e4ba9c090ca
Jakub Španger <jakubspanger1025@gmail.com>
2023-12-08 20:32:02 +0100

    BetterAnalysis: offset

--- GlobalQuakeCore/src/main/java/globalquake/core/analysis/BetterAnalysis.java
+++ GlobalQuakeCore/src/main/java/globalquake/core/analysis/BetterAnalysis.java
@@ -30,6 +30,8 @@ public class BetterAnalysis extends Analysis {
     private double specialAverage;
     private double thirdAverage;
     private long eventTimer;
+    private double offset;
+
 
 
     public static final double min_frequency = 2.0;
@@ -108,6 +110,7 @@ public class BetterAnalysis extends Analysis {
         shortAverage -= (shortAverage - absFilteredV) / (getSampleRate() * 0.5);
         mediumAverage -= (mediumAverage - absFilteredV) / (getSampleRate() * 6.0);
         thirdAverage -= (thirdAverage - absFilteredV) / (getSampleRate() * 30.0);
+        offset -= (offset - filteredV) / (getSampleRate() * 30.0);
 
         if (absFilteredV > specialAverage) {
             specialAverage = absFilteredV;
@@ -162,7 +165,7 @@ public class BetterAnalysis extends Analysis {
             sensitivity = DEFAULT_SENSITIVITY;
         }
 
-        double counts = filteredV * (DEFAULT_SENSITIVITY / sensitivity);
+        double counts = (filteredV - offset) * (DEFAULT_SENSITIVITY / sensitivity);
 
         countsSum += counts / getSampleRate();
         countsSum *= 0.999;
@@ -230,6 +233,7 @@ public class BetterAnalysis extends Analysis {
     public void reset() {
         _maxRatio = 0;
         _maxCounts = 0;
+        offset = 0.0;
         setStatus(AnalysisStatus.INIT);
         initProgress = 0;
         initialOffsetSum = 0;
