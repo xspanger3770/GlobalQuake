f5156e46b393ede56721cbd1ef3f26e8935a0167
Jakub Španger <jakubspanger1025@gmail.com>
2023-12-15 21:53:14 +0100

    pre18

--- GlobalQuakeCore/src/main/java/globalquake/core/GlobalQuake.java
+++ GlobalQuakeCore/src/main/java/globalquake/core/GlobalQuake.java
@@ -18,7 +18,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class GlobalQuake {
-	public static final String version = "v0.10.0_pre17";
+	public static final String version = "v0.10.0_pre18";
 
 	protected GlobalQuakeRuntime globalQuakeRuntime;
 	protected SeedlinkNetworksReader seedlinkNetworksReader;
