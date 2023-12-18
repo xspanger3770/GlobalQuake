86e90b0d8a566ecfa56d422324b5af96a133d05d
Samuel Hliva <samuel.hliva@student.tuke.sk>
2023-12-18 20:47:47 +0100

    import/export optimisation

--- GlobalQuakeCore/src/main/java/globalquake/utils/LookupTableIO.java
+++ GlobalQuakeCore/src/main/java/globalquake/utils/LookupTableIO.java
@@ -1,11 +1,10 @@
 package globalquake.utils;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
+
 import globalquake.core.regions.Regions;
 
 public class LookupTableIO {
@@ -19,60 +18,41 @@ public class LookupTableIO {
         return performExport(lookupTable);
     }
 
-    public static boolean exportLookupTableToFile(HashMap<String, Double> lookupTable){
+    public static boolean exportLookupTableToFile(HashMap<String, Double> lookupTable) throws IOException {
         return performExport(lookupTable);
     }
 
     private static boolean performExport(HashMap<String, Double> lookupTable) {
-        String fileName = "lookupTable.dat";
-        String resourcesDir = "./src/main/resources/lookup/";
+        String path = "lookup/lookupTable.dat";
+        URL resource = ClassLoader.getSystemClassLoader().getResource(path);
 
-        try {
-            Path filePath = Paths.get(resourcesDir + fileName);
-            File file = filePath.toFile();
-            if (!file.exists()) {
-                Files.createDirectories(filePath.getParent());
-                Files.createFile(filePath);
-            }
-
-            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
-                oos.writeObject(lookupTable);
-                System.out.println("Lookup table exported to file successfully.");
-                return true;
-            } catch (IOException e) {
-                System.err.println("Error exporting lookup table: " + e.getMessage());
-            }
-        } catch (IOException e) {
-            System.err.println("Error creating file: " + e.getMessage());
+        try{
+            ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("lookupTable.dat"));
+            output.writeObject(lookupTable);
+            output.close();
+        } catch (Exception e){
+            System.err.println("Unable to save a lookup table! " + e);
+            return false;
         }
-        return false;
 
+        return true;
     }
 
+    public static HashMap<String, Double> importLookupTableFromFile() throws IOException {
+        String path = "lookup/lookupTable.dat";
+        URL resource = ClassLoader.getSystemClassLoader().getResource(path);
 
-
-    public static HashMap<String, Double> importLookupTableFromFile() {
-        HashMap<String, Double> lookupTable = new HashMap<>();
-        String fileName = "lookupTable.dat";
-        String resourcesDir = "./src/main/resources/lookup/";
-
-        try {
-            Path filePath = Paths.get(resourcesDir + fileName);
-            File file = filePath.toFile();
-            if (file.exists()) {
-                try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
-                    lookupTable = (HashMap<String, Double>) ois.readObject();
-                    System.out.println("Lookup table imported from file successfully.");
-                } catch (IOException | ClassNotFoundException e) {
-                    System.err.println("Error importing lookup table: " + e.getMessage());
-                }
-            } else {
-                System.err.println("File not found: " + fileName);
-                return null;
-            }
-        } catch (Exception e) {
-            System.err.println("Error accessing file: " + e.getMessage());
+        if (resource == null) {
+            System.err.printf("Unable to load a lookup table: %s", path);
             return null;
+        }
+
+        HashMap<String, Double> lookupTable;
+        try{
+            ObjectInput input = new ObjectInputStream(resource.openStream());
+            lookupTable = (HashMap<String, Double>) input.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new IOException("Unable to load stream of a lookup table! ", e);
         }
 
         return lookupTable;
