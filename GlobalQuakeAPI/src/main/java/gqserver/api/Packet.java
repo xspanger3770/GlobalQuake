58b21a8b36c8bda841fa4f04ed014c243a47c922
Jakub Španger <jakubspanger1025@gmail.com>
2024-01-23 19:08:34 +0100

    try to fix serialisation

--- GlobalQuakeAPI/src/main/java/gqserver/api/Packet.java
+++ GlobalQuakeAPI/src/main/java/gqserver/api/Packet.java
@@ -1,9 +1,13 @@
 package gqserver.api;
 
 import java.io.IOException;
+import java.io.Serial;
 import java.io.Serializable;
 
 public interface Packet extends Serializable {
+
+    @Serial
+    public static final long serialVersionUID = 0L;
 
     default void onServerReceive(ServerClient serverClient) throws IOException {}
 
