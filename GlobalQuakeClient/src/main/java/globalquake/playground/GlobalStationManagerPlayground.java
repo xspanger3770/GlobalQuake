9dfad2bbc1ccd9dfec1ca998ddd9a85f9e820608
Jakub Španger <jakubspanger1025@gmail.com>
2024-01-28 22:14:38 +0100

    why the hell git didn't automatically add this

---
+++ GlobalQuakeClient/src/main/java/globalquake/playground/GlobalStationManagerPlayground.java
@@ -0,0 +1,12 @@
+package globalquake.playground;
+
+import globalquake.core.database.StationDatabaseManager;
+import globalquake.core.station.GlobalStationManager;
+
+public class GlobalStationManagerPlayground extends GlobalStationManager {
+
+    @Override
+    public void initStations(StationDatabaseManager databaseManager) {
+        super.initStations(databaseManager);
+    }
+}
