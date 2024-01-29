f0b854e4da72fc102bdec30f5c0ada6ae4c7f435
Jakub Španger <jakubspanger1025@gmail.com>
2024-01-29 16:10:28 +0100

    better bot

--- GlobalQuakeServer/src/main/java/gqserver/bot/DiscordBot.java
+++ GlobalQuakeServer/src/main/java/gqserver/bot/DiscordBot.java
@@ -7,6 +7,11 @@ import globalquake.core.events.specific.QuakeCreateEvent;
 import globalquake.core.events.specific.QuakeRemoveEvent;
 import globalquake.core.events.specific.QuakeReportEvent;
 import globalquake.core.events.specific.QuakeUpdateEvent;
+import globalquake.core.geo.DistanceUnit;
+import globalquake.core.intensity.IntensityScales;
+import globalquake.core.intensity.Level;
+import globalquake.core.intensity.MMIIntensityScale;
+import globalquake.utils.GeoUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -14,9 +19,18 @@ import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import javax.imageio.ImageIO;
 import javax.security.auth.login.LoginException;
 
 import globalquake.core.Settings;
+import net.dv8tion.jda.api.utils.FileUpload;
+import org.tinylog.Logger;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.time.Instant;
 
 public class DiscordBot extends ListenerAdapter{
 
@@ -54,19 +68,70 @@ public class DiscordBot extends ListenerAdapter{
         if(jda == null){
             return;
         }
+
+        var channel = jda.getGuildById(Settings.discordBotGuildID).getTextChannelById(Settings.discordBotChannelID);
+        if(channel == null){
+            return;
+        }
+
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setAuthor("Final Report");
+
+        builder.setImage("attachment://map.png");
+        builder.setThumbnail("attachment://int.png");
+        createDescription(builder, event.earthquake());
+
+        ByteArrayOutputStream baosMap = new ByteArrayOutputStream();
+        ByteArrayOutputStream baosInt = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(event.map(), "png", baosMap);
+            ImageIO.write(event.intensities(), "png", baosInt);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        channel.sendMessageEmbeds(builder.build())
+                .addFiles(FileUpload.fromData(baosMap.toByteArray(), "map.png"), FileUpload.fromData(baosInt.toByteArray(), "int.png"))
+                .queue();
+
     }
 
     private static void sendQuakeRemoveInfo(Earthquake earthquake) {
         if(jda == null){
             return;
         }
+
+        var channel = jda.getGuildById(Settings.discordBotGuildID).getTextChannelById(Settings.discordBotChannelID);
+
+        if(channel == null){
+            return;
+        }
+
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setTitle("M%.1f %s".formatted(earthquake.getMag(), earthquake.getRegion()));
+        builder.setAuthor("CANCELED");
+
+        channel.sendMessageEmbeds(builder.build()).queue();
     }
 
     private static void sendQuakeUpdateInfo(Earthquake earthquake) {
+        if(true){
+            return;
+        }
         if(jda == null){
             return;
         }
 
+        var channel = jda.getGuildById(Settings.discordBotGuildID).getTextChannelById(Settings.discordBotChannelID);
+        if(channel == null){
+            return;
+        }
+
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setAuthor("Revision #%d".formatted(earthquake.getRevisionID()));
+        createDescription(builder, earthquake);
+
+        channel.sendMessageEmbeds(builder.build()).queue();
     }
 
     private static void sendQuakeCreateInfo(Earthquake earthquake) {
@@ -75,24 +140,51 @@ public class DiscordBot extends ListenerAdapter{
         }
 
         var channel = jda.getGuildById(Settings.discordBotGuildID).getTextChannelById(Settings.discordBotChannelID);
+        if(channel == null){
+            return;
+        }
+
         EmbedBuilder builder = new EmbedBuilder();
-        builder.setTitle("M%.1f %s".formatted(earthquake.getMag(), earthquake.getRegion()));
-        builder.setDescription("Description of the Embed");
-        builder.appendDescription("asdasda");
+        builder.setAuthor("New Event");
+        createDescription(builder, earthquake);
 
         channel.sendMessageEmbeds(builder.build()).queue();
     }
 
+    private static void createDescription(EmbedBuilder builder, Earthquake earthquake) {
+        builder.setTitle("M%.1f %s".formatted(
+                earthquake.getMag(),
+                earthquake.getRegion()));
+
+        builder.setDescription(
+                "Depth: %.1fkm / %.1fmi\n".formatted(earthquake.getDepth(), earthquake.getDepth() * DistanceUnit.MI.getKmRatio()) +
+                "Time: %s\n".formatted(Settings.formatDateTime(Instant.ofEpochMilli(earthquake.getOrigin()))) +
+                "Quality: %s (%d stations)".formatted(earthquake.getCluster().getPreviousHypocenter().quality.getSummary(), earthquake.getCluster().getAssignedEvents().size())
+        );
+
+        double pga = GeoUtils.pgaFunction(earthquake.getMag(), 0, earthquake.getDepth());
+
+        Level level = IntensityScales.getIntensityScale().getLevel(pga);
+        Color levelColor = level == null ? Color.gray : level.getColor();
+
+        builder.setColor(levelColor);
+        builder.setFooter("Created at: %s".formatted(Settings.formatDateTime(Instant.now())));
+    }
+
     @Override
     public void onReady(ReadyEvent event) {
-        System.out.println("Bot is ready!");
         var channel = jda.getGuildById(Settings.discordBotGuildID).getTextChannelById(Settings.discordBotChannelID);
+        if(channel == null){
+            return;
+        }
+
         EmbedBuilder builder = new EmbedBuilder();
-        builder.setTitle("title");
-        builder.setDescription("Description of the Embed");
-        builder.appendDescription("asdasda");
+        builder.setTitle("GlobalQuake BOT");
+        builder.setDescription("Initialising...");
 
         channel.sendMessageEmbeds(builder.build()).queue();
+
+        Logger.info("Discord bot ready");
     }
 
 }
