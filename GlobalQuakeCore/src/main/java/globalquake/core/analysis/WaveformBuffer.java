e884c3f0a822a303ffb91b8754d88e4cb393914b
Jakub Španger <jakubspanger1025@gmail.com>
2024-02-01 18:28:53 +0100

    WaveformBuffer improvements

--- GlobalQuakeCore/src/main/java/globalquake/core/analysis/WaveformBuffer.java
+++ GlobalQuakeCore/src/main/java/globalquake/core/analysis/WaveformBuffer.java
@@ -1,9 +1,7 @@
 package globalquake.core.analysis;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.List;
 
 public class WaveformBuffer {
     public static final int COMPUTED_COUNT = 5;
@@ -12,6 +10,7 @@ public class WaveformBuffer {
     public static final int MEDIUM_AVERAGE = 2;
     public static final int LONG_AVERAGE = 3;
     public static final int SPECIAL_AVERAGE = 4;
+    private final double sps;
 
 
     private int size;
@@ -24,13 +23,19 @@ public class WaveformBuffer {
     private int oldestDataSlot = 0;
 
     public WaveformBuffer(double sps, int seconds) {
+        this.sps = sps;
         this.size = (int) Math.ceil(seconds * sps);
+
+        if(size <= 0){
+            throw new IllegalArgumentException("Wavefor buffer size must be positive!");
+        }
+
         rawValues = new int[size];
         computed = new float[COMPUTED_COUNT][size];
         times = new long[size];
         this.lastLog = Long.MIN_VALUE;
         this.nextFreeSlot = 0;
-        this.oldestDataSlot = size - 1;
+        this.oldestDataSlot = 0;
     }
 
     public void log(long time, int rawValue, float filteredV, float shortAverage, float mediumAverage, float longAverage,
@@ -38,8 +43,6 @@ public class WaveformBuffer {
         if(time <= lastLog) {
             return;
         }
-
-        lastLog = time;
 
         times[nextFreeSlot] = time;
         rawValues[nextFreeSlot] = rawValue;
@@ -49,14 +52,52 @@ public class WaveformBuffer {
         computed[LONG_AVERAGE][nextFreeSlot] = longAverage;
         computed[SPECIAL_AVERAGE][nextFreeSlot] = specialAverage;
 
-        if(nextFreeSlot == oldestDataSlot){
+        if(nextFreeSlot == oldestDataSlot && !isEmpty()) {
             oldestDataSlot = (oldestDataSlot + 1) % size;
         }
         nextFreeSlot = (nextFreeSlot + 1) % size;
+
+        lastLog = time;
     }
 
     public void resize(int seconds) {
-        // todo
+        int new_size = (int) Math.ceil(seconds * sps);
+        _resize(new_size);
+    }
+
+    private void _resize(int new_size) {
+        int[] new_rawValues = new int[new_size];
+        float[][] new_computed = new float[COMPUTED_COUNT][new_size];
+        long[] new_times = new long[new_size];
+
+        int i2 = 0;
+        for(int step = 0; step < Math.min(size, new_size); step++){
+            i2 -= 1;
+            if(i2 < 0){
+                i2 = new_size - 1;
+            }
+
+            nextFreeSlot -= 1;
+            if(nextFreeSlot < 0){
+                nextFreeSlot = size - 1;
+            }
+
+            new_times[i2] = times[nextFreeSlot];
+            new_rawValues[i2] = rawValues[nextFreeSlot];
+            new_computed[0][i2] = computed[0][nextFreeSlot];
+            new_computed[1][i2] = computed[1][nextFreeSlot];
+            new_computed[2][i2] = computed[2][nextFreeSlot];
+            new_computed[3][i2] = computed[3][nextFreeSlot];
+            new_computed[4][i2] = computed[4][nextFreeSlot];
+        }
+
+        this.times = new_times;
+        this.rawValues = new_rawValues;
+        this.computed = new_computed;
+
+        this.oldestDataSlot = i2;
+        this.nextFreeSlot = 0;
+        this.size = new_size;
     }
 
     public int getSize() {
@@ -71,12 +112,35 @@ public class WaveformBuffer {
         return lastLog == Long.MIN_VALUE;
     }
 
-    public int getNextFreeSlot() {
+    public int getNextSlot() {
         return nextFreeSlot;
     }
 
     public int getOldestDataSlot() {
         return oldestDataSlot;
+    }
+
+    public long getTime(int index){
+        return times[index];
+    }
+
+    public int getRaw(int index){
+        return rawValues[index];
+    }
+
+    public float getComputed(int type, int index){
+        return computed[type][index];
+    }
+
+    public Log toLog(int index){
+        return new Log(
+                times[index],
+                rawValues[index],
+                computed[0][index],
+                computed[1][index],
+                computed[2][index],
+                computed[3][index],
+                computed[4][index]);
     }
 
     public static void main(String[] args) {
@@ -89,4 +153,8 @@ public class WaveformBuffer {
         System.err.println("a");
     }
 
+    public int getNewestDataSlot() {
+        int res = nextFreeSlot - 1;
+        return res >= 0 ? res : size - 1;
+    }
 }
--- GlobalQuakeCore/src/test/java/globalquake/core/analysis/WaveformBufferTest.java
+++ GlobalQuakeCore/src/test/java/globalquake/core/analysis/WaveformBufferTest.java
@@ -11,7 +11,7 @@ public class WaveformBufferTest {
         double sps = 30.0;
         int seconds = 10;
         WaveformBuffer waveformBuffer = new WaveformBuffer(sps, seconds);
-        assertEquals(waveformBuffer.getSize(), 300);
+        assertEquals(300, waveformBuffer.getSize());
     }
 
     @Test
@@ -19,23 +19,68 @@ public class WaveformBufferTest {
         double sps = 1.0;
         int seconds = 10;
         WaveformBuffer waveformBuffer = new WaveformBuffer(sps, seconds);
-        assertEquals(waveformBuffer.getSize(), 10);
-        assertEquals(waveformBuffer.getNextFreeSlot(), 0);
-        assertEquals(waveformBuffer.getOldestDataSlot(), 9);
+        assertEquals(10, waveformBuffer.getSize());
+        assertEquals(0, waveformBuffer.getNextSlot());
+        assertEquals(0, waveformBuffer.getOldestDataSlot());
         assertTrue(waveformBuffer.isEmpty());
 
         waveformBuffer.log(0,0,0,0,0,0,0);
-        assertEquals(waveformBuffer.getNextFreeSlot(), 1);
-        assertEquals(waveformBuffer.getOldestDataSlot(), 9);
+        assertEquals(1, waveformBuffer.getNextSlot());
+        assertEquals(0, waveformBuffer.getOldestDataSlot());
 
         for(int i = 0; i < 9; i++){
             waveformBuffer.log(i + 1,0,0,0,0,0,0);
         }
-        assertEquals(waveformBuffer.getNextFreeSlot(), 0);
+        assertEquals(waveformBuffer.getNextSlot(), 0);
         assertEquals(waveformBuffer.getOldestDataSlot(), 0);
         waveformBuffer.log(10,0,0,0,0,0,0);
-        assertEquals(waveformBuffer.getNextFreeSlot(), 1);
+        assertEquals(waveformBuffer.getNextSlot(), 1);
         assertEquals(waveformBuffer.getOldestDataSlot(), 1);
+    }
+
+    @Test
+    public void testStorage(){
+        double sps = 1.0;
+        int seconds = 10;
+        WaveformBuffer waveformBuffer = new WaveformBuffer(sps, seconds);
+        waveformBuffer.log(0,10,10,10,10,10,10);
+
+        Log log0 = waveformBuffer.toLog(0);
+
+        assertEquals(log0.time(), 0);
+        assertEquals(log0.shortAverage(), 10, 1e-6);
+    }
+
+    @Test
+    public void testResize(){
+        double sps = 1.0;
+        int seconds = 10;
+        WaveformBuffer waveformBuffer = new WaveformBuffer(sps, seconds);
+
+        for(int i = 0; i < waveformBuffer.getSize(); i++) {
+            waveformBuffer.log(i, i*10, i*20, i*30, i*40, i*50, i*60);
+        }
+
+        waveformBuffer.resize(3);
+
+        assertEquals(3, waveformBuffer.getSize());
+
+        Log log0 = waveformBuffer.toLog(waveformBuffer.getOldestDataSlot());
+
+        assertEquals(7, log0.time());
+        assertEquals(30*7, log0.shortAverage(), 1e-6);
+
+        waveformBuffer.log(100,100,100,100,100,100,100);
+
+        log0 = waveformBuffer.toLog(waveformBuffer.getOldestDataSlot());
+
+        assertEquals(8, log0.time());
+        assertEquals(30*8, log0.shortAverage(), 1e-6);
+
+        log0 = waveformBuffer.toLog(waveformBuffer.getNewestDataSlot());
+
+        assertEquals(100, log0.time());
+        assertEquals(100, log0.shortAverage(), 1e-6);
     }
 
 }
