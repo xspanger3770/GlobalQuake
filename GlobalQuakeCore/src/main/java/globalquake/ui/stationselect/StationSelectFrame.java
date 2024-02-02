128cb13d27ad06bc0fc976822136bbf8792c9bf9
ChrisEberleSchool <156865868+ChrisEberleSchool@users.noreply.github.com>
2024-02-02 05:06:34 -0700

    - removed unneccessary logic
    - no need to grey out deselectall and selectall buttons.
    - this just ensure you are always able to selectall and deselectall

--- GlobalQuakeCore/src/main/java/globalquake/ui/stationselect/StationSelectFrame.java
+++ GlobalQuakeCore/src/main/java/globalquake/ui/stationselect/StationSelectFrame.java
@@ -193,25 +193,5 @@ public class StationSelectFrame extends GQFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == deselectAll){
-            selectAll.setEnabled(true);
-            selectButton.setEnabled(true);
-            deselectAll.setEnabled(false);
-            deselectButton.setEnabled(false);
-            setDragMode(DragMode.NONE);
-        }
-        else if(e.getSource() == selectAll){
-            selectAll.setEnabled(false);
-            selectButton.setEnabled(false);
-            deselectAll.setEnabled(true);
-            deselectButton.setEnabled(true);
-            setDragMode(DragMode.NONE);
-        }
-        else{
-            selectAll.setEnabled(true);
-            selectButton.setEnabled(true);
-            deselectAll.setEnabled(true);
-            deselectButton.setEnabled(true);
-        }
     }
 }
