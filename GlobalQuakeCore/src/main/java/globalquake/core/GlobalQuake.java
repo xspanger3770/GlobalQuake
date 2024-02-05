937d8957c8cda39ee3ddd0c411c8ec83d98b83d7
Jakub Španger <jakubspanger1025@gmail.com>
2024-02-05 10:52:38 +0100

    build-22

--- GlobalQuakeCore/src/main/java/globalquake/core/GlobalQuake.java
+++ GlobalQuakeCore/src/main/java/globalquake/core/GlobalQuake.java
@@ -19,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 
 public abstract class GlobalQuake {
 
-    public static final String version = "v0.10.2_build-21";
+    public static final String version = "v0.10.2_build-22";
 
     protected GlobalQuakeRuntime globalQuakeRuntime;
     protected SeedlinkNetworksReader seedlinkNetworksReader;
